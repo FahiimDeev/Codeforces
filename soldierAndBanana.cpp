e47a1a266615701a4951ffebcc5e16cf2eca0ed3
Mustafa Fahim <91342968+FahiimDeev@users.noreply.github.com>
2024-02-13 18:44:13 +0600

    Add files via upload

--- soldierAndBanana.cpp
+++ soldierAndBanana.cpp
@@ -3,10 +3,19 @@ using namespace std;
 
 int main(){
 
-    int k,n,w;
-    cin>>k>>n>>w;
-    
-    
+    int price,dollars,turns, borrow;
+    cin>>price>>dollars>>turns;
+
+     int total_cost = price * (turns * (turns + 1) / 2);
+
+     if (total_cost > dollars){
+        borrow = total_cost - dollars;
+        cout<<borrow;
+     }
+     else {
+        cout<<0<<endl;
+     }
+
 
     return 0;
 }
